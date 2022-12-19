5d52ca7740a4525a59c3d98f9f7353e34c239d84
ganeshardlkar <ganeshardalkar5@gmail.com>
2022-12-19 13:59:45 +0530

    Added filters to get all cities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { City } = require('../models/index');
 
 class CityRepository {
@@ -34,7 +35,7 @@ class CityRepository {
             const city = await City.findByPk(cityId);
             city.name = data.name;
             await city.save();
-            
+
             return city;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
@@ -46,6 +47,26 @@ class CityRepository {
         try {
             const city = City.findByPk(cityId);
             return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw(error);
+        }
+    }
+
+    async getAllCities(filter) { // filter can be empty
+        try {
+            if(filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                });
+                return cities;
+            }
+            const cities = await City.findAll();
+            return cities;
         } catch (error) {
             console.log("Something went wrong in the repository layer");
             throw(error);

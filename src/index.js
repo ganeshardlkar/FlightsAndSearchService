8e2cbc447727ca604556592bf4c2eaad0eb6e3d2
ganeshardlkar <ganeshardalkar5@gmail.com>
2022-12-20 13:53:24 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
+const db = require('./models/index');
+const {Airplane} = require('./models/index');
+
 const setupAndStartServer = async () => {
     //create the express object
     const app = express();
@@ -13,8 +16,15 @@ const setupAndStartServer = async () => {
 
     app.use('/api', ApiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server is listening on ${PORT}`); 
+        
+        if(process.env.SYNC_DB) {
+            db.sequelize.sync({alter: true});
+        }
+        await Airplane.create({
+            modelNumber: 'Bombardier CRJ'
+        })
     });
 }
 
